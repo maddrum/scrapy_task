56cc172af27d87a16845dd32852f3255b689fa60
maddrum <maddrum9@gmail.com>
2020-09-19 00:03:00 +0300

    databases initial

--- django_scrapy_task/crawler_app/admin.py
+++ django_scrapy_task/crawler_app/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import ScrapedData
 
-# Register your models here.
+
+class ReadOnlyFields(admin.ModelAdmin):
+    readonly_fields = ('created_on', 'edited_on')
+
+
+admin.site.register(ScrapedData, ReadOnlyFields)
--- django_scrapy_task/crawler_app/models.py
+++ django_scrapy_task/crawler_app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class ScrapedData(models.Model):
+    name = models.CharField(max_length=255)
+    avatar = models.URLField()
+    country_of_birth = models.CharField(max_length=100)
+    place_of_birth = models.CharField(max_length=255)
+    date_of_birth = models.DateField()
+    job = models.CharField(max_length=100)
+    languages = models.CharField(max_length=255)
+    political_party = models.CharField(max_length=255)
+    email = models.EmailField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    edited_on = models.DateField(auto_now=True)
